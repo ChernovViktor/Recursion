3315b75d10df34803599be0ae4c05cb5bd58b543
chern <chern@DESKTOP-GDTMGA7>
2021-05-05 20:57:11 +0300

    1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,7 @@
 ﻿/*
 ------------------------------------------------------------------------------------------
-				  РЕКУРСИЯ
-__________________________________________________________________________________________
+				                       РЕКУРСИЯ
+------------------------------------------------------------------------------------------
 
 Рекурсией называется ситуация, когда функция вызывает сама себя непосредственно или через другую функцию
 Рекурския нужна для организации циклических действий. То есть есть 2 подхода к решению задачи
@@ -16,11 +16,23 @@ ________________________________________________________________________________
  есть сегмент кода, сегмент данных, сегмент стэка. Так же есть контейнер стэк, но при вызове функции состояние вызывающей функции сохранчется в стэке
  а так же параметры передаются в функцию через стэк
 
+ Когда мы пишем цикл, мы в первую очередь думаем об условии выхода из цикла.
+ Когда мы пишем рекурсивную функцию, мы в первую очередь должны подумать об условии выхода из рекурсии, а во вторую о рекурсивном вызове
+
 */
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 //#define INT1
+//#define INT2
+//#define FACTORIAL
+//#define STEPEN
+
+void elevator(int floor);
+int factorial(int n);
+double power(double n, int step);
+
 
 #ifdef INT1
 int main()
@@ -34,16 +46,76 @@ int main()
 	}
 
 }
-#endif 
-
+#endif //INT1
+#ifdef INT2
 int main()
 {
 	setlocale(LC_ALL, "ru");
+	system("cls");
+	cout << "Hello World!\n";
 
-	while (true)
+	int floor;
+	cout << "На каком вы этаже" << endl;  cin >> floor;
+	elevator(floor);
+	cout << "Еще разочек? (y/n)";
+	if (_getch() == 'y')
 	{
-		cout << "Hello World!\n";
+		main();
 	}
-	main();
+	// _getch() - ожидает нажатие клавиши = и возваращает ASCII-код нажатой клавиши 
+
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "\nпривет подвал!\n\n";
+		return;
+	}
+	static int counter = 0;
+	cout << "Мы на " << floor << " этаже" << "\t";
+	cout << counter++ << endl;
+	elevator(floor-1);
+	cout << counter++ << endl;
+	cout << "Мы на " << floor << " этаже" << "\t";
+
+}
+}
+#endif // INT2
+#ifdef FACTORIAL
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Введите число\n";
+	int n = 0; cin >> n;
+	cout << factorial(n);
+
+}
+#endif // FACTORIAL
+#ifdef FACTORIAL
+int factorial(int n)
+{
+	if (n == 0) return 1;
+
+	int f = n * factorial(n - 1);
+	return f;
+}
+#endif //FACTORIAL
+#ifdef STEPEN
+int main()
+{
+	cout << "Vvedite n i step\n";
+	int n = 0; cin >> n;
+	int step = 0; cin >> step;
+	cout << power(n, step);
+}
+
+double power(double n, int step)
+{
+	return step == 0 ? 1 : step < 0 ? 1 / n * power(n, step + 1) : n * power(n, step - 1);
+}
+#endif // STEPEN
+
+int main()
+{
 
 }
